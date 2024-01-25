29b820887c500f5550222acdd1eff0751fa94c5a
tnischal <itstnischal@gmail.com>
2024-01-25 16:06:45 -0600

    EveryThing is fine and fetchall controller is created in account controller

--- src/main/java/com/eazybyte/accounts/DTO/AccountContactDTO.java
+++ src/main/java/com/eazybyte/accounts/DTO/AccountContactDTO.java
@@ -1,9 +1,16 @@
 package com.eazybyte.accounts.DTO;
 
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
 import java.util.Map;
 @ConfigurationProperties(prefix = "accounts")
-public record AccountContactDTO(String message, Map<String, String> contactDetails, List<String> onCallSupport) {
+@Getter @Setter
+public class AccountContactDTO {
+
+    String message;
+    Map<String, String> contactDetails;
+    List<String> onCallSupport;
 }
